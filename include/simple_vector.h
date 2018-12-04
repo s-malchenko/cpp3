36dcb5dba5c9b8a8d4c06a949fe99fb679c13f12
s-malchenko <s.malchenko@dreamkas.ru>
2018-12-04 18:30:59 +0300

    simple_vector done

---
+++ include/simple_vector.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include <cstdlib>
+#include <algorithm>
+
+// Реализуйте шаблон SimpleVector
+template <typename T>
+class SimpleVector
+{
+public:
+    SimpleVector()
+    {
+        begin_ = nullptr;
+        end_ = nullptr;
+        capacity_ = 0;
+    }
+
+    explicit SimpleVector(size_t size)
+    {
+        begin_ = new T[size];
+        end_ = begin_ + size;
+        capacity_ = size;
+    }
+
+    ~SimpleVector()
+    {
+        delete[] begin_;
+    }
+
+    T &operator[](size_t index)
+    {
+        return *(begin_ + index);
+    }
+
+    const T &operator[](size_t index) const
+    {
+        return *(begin_ + index);
+    }
+
+    T *begin() { return begin_; }
+    T *end() { return end_; }
+    const T *begin() const { return begin_; }
+    const T *end() const { return end_; }
+
+    size_t Size() const
+    {
+        return end_ - begin_;
+    }
+
+    size_t Capacity() const
+    {
+        return capacity_;
+    }
+
+    void PushBack(const T &value)
+    {
+        if (capacity_ == 0)
+        {
+            capacity_ = 1;
+            begin_ = new T[1];
+            end_ = begin_ ;
+        }
+        else if (capacity_ - Size() < 1)
+        {
+            capacity_ <<= 1;
+            T *newBegin = new T[capacity_];
+            std::copy(begin_, end_, newBegin);
+            delete[] begin_;
+            end_ = newBegin + Size();
+            begin_ = newBegin;
+        }
+
+        *(end_++) = value;
+    }
+
+private:
+    // Добавьте поля для хранения данных вектора
+    T *begin_;
+    T *end_;
+    size_t capacity_;
+};
