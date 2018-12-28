aef881c9f57e0ae99865ae6ac38e5d40da4ad778
s-malchenko <s.malchenko@dreamkas.ru>
2018-12-28 19:07:41 +0300

    priority_collection in progress

--- priority_collection.cpp
+++ priority_collection.cpp
@@ -3,9 +3,8 @@
 #include <iostream>
 #include <iterator>
 #include <memory>
-#include <set>
 #include <utility>
-#include <vector>
+#include <list>
 
 using namespace std;
 
@@ -13,37 +12,104 @@ template <typename T>
 class PriorityCollection
 {
 public:
-    using Id = /* тип, используемый для идентификаторов */;
+    using Entry = pair<T, int>;
+    using Id = typename list<pair<T, int>>::const_iterator;
 
     // Добавить объект с нулевым приоритетом
     // с помощью перемещения и вернуть его идентификатор
-    Id Add(T object);
+    Id Add(T object)
+    {
+        return addElement(object);
+    }
 
     // Добавить все элементы диапазона [range_begin, range_end)
     // с помощью перемещения, записав выданные им идентификаторы
     // в диапазон [ids_begin, ...)
     template <typename ObjInputIt, typename IdOutputIt>
     void Add(ObjInputIt range_begin, ObjInputIt range_end,
-             IdOutputIt ids_begin);
+             IdOutputIt ids_begin)
+    {
+        for (auto it = range_begin; it != range_end; ++it)
+        {
+            ids_begin = addElement(*it);
+            ++ids_begin;
+        }
+    }
 
     // Определить, принадлежит ли идентификатор какому-либо
     // хранящемуся в контейнере объекту
-    bool IsValid(Id id) const;
+    bool IsValid(Id id) const
+    {
+        for (auto i = data.begin(); i != data.end(); ++i)
+        {
+            if (i == id)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
 
     // Получить объект по идентификатору
-    const T &Get(Id id) const;
+    const T &Get(Id id) const
+    {
+        return id->first;
+    }
 
     // Увеличить приоритет объекта на 1
-    void Promote(Id id);
+    void Promote(Id id)
+    {
+        ++(convertId(id)->second);
+    }
 
     // Получить объект с максимальным приоритетом и его приоритет
-    pair<const T &, int> GetMax() const;
+    pair<const T &, int> GetMax() const
+    {
+        auto id = getMaxId();
+        return {id->first, id->second};
+    }
 
     // Аналогично GetMax, но удаляет элемент из контейнера
-    pair<T, int> PopMax();
+    pair<T, int> PopMax()
+    {
+        auto id = convertId(getMaxId());
+        pair<T, int> element = {move(id->first), id->second};
+        data.erase(id);
+        return element;
+    }
 
 private:
     // Приватные поля и методы
+    list<Entry> data;
+
+    Id addElement(T &element)
+    {
+        data.push_back({move(element), 0});
+        return --data.end();
+    }
+
+    Id getMaxId() const
+    {
+        Id result = data.begin();
+        int maxPriority = data.front().second;
+
+        for (auto it = data.begin(); it != data.end(); ++it)
+        {
+            if (it->second >= maxPriority)
+            {
+                maxPriority = it->second;
+                result = it;
+            }
+        }
+
+        return result;
+    }
+
+    typename list<pair<T, int>>::iterator convertId(const Id id)
+    {
+        return data.erase(id, id);
+    }
 };
 
 
@@ -87,9 +153,21 @@ void TestNoCopy()
     }
 }
 
+void TestConst()
+{
+    PriorityCollection<StringNonCopyable> strings;
+    const auto white_id = strings.Add("white");
+    const auto yellow_id = strings.Add("yellow");
+    const auto red_id = strings.Add("red");
+
+    const auto &constStrings = strings;
+    constStrings.GetMax();
+}
+
 int main()
 {
     TestRunner tr;
     RUN_TEST(tr, TestNoCopy);
+    RUN_TEST(tr, TestConst);
     return 0;
 }
