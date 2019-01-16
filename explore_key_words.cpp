f3c148b3e2982a40ce4f30f10f127c08a395aa2e
s-malchenko <s.malchenko@dreamkas.ru>
2019-01-16 13:46:57 +0300

    explore_key_words done

--- explore_key_words.cpp
+++ explore_key_words.cpp
@@ -5,7 +5,6 @@
 #include <string>
 #include <future>
 #include <functional>
-#include <iostream>
 #include <sstream>
 #include <list>
 using namespace std;
@@ -68,20 +67,30 @@ Stats ExploreKeyWords(const set<string> &key_words, istream &input)
     Stats result;
     vector<future<Stats>> v;
     list<stringstream> streams;
-    const size_t threadLines = 100000;
+    const size_t threadLines = 12500;
 
     while (input)
     {
         stringstream ss;
-        size_t i = threadLines;
+        size_t i;
 
-        for (string line; getline(input, line) && i; --i)
+        for (i = 0; i < threadLines; ++i)
         {
+            string line;
+
+            if (!getline(input, line))
+            {
+                break;
+            }
+
             ss << move(line) << "\n";
         }
 
-        streams.push_back(move(ss));
-        v.push_back(async(ExploreKeyWordsSingleThread, ref(key_words), ref(streams.back())));
+        if (i)
+        {
+            streams.push_back(move(ss));
+            v.push_back(async(ExploreKeyWordsSingleThread, ref(key_words), ref(streams.back())));
+        }
     }
 
     for (auto &i : v)
@@ -94,28 +103,42 @@ Stats ExploreKeyWords(const set<string> &key_words, istream &input)
 
 void TestBasic()
 {
-    const set<string> key_words = {"yangle", "rocks", "sucks", "all"};
+    set<string> key_words = {"yangle", "rocks", "sucks", "all"};
+    const int repeatNum = 10000;
+    const int additionalKeywords = 50000;
 
+    map<string, int> expected =
+    {
+        {"yangle", 6 * repeatNum},
+        {"rocks", 2 * repeatNum},
+        {"sucks", 1 * repeatNum}
+    };
+
+    for (auto i = 0; i < additionalKeywords; ++i)
+    {
+        stringstream ss;
+        ss << "word" << i << "\n";
+        string word;
+        ss >> word;
+        key_words.insert(word);
+        // expected.insert({word, 0});
+    }
+
+    const string filler = "word word word word word word word word word word word word word word word word word word word word ";
     stringstream ss;
-    const int repeatNum = 100000;
     for (int i = 0; i < repeatNum; ++i)
     {
-        ss << "this new yangle service really rocks\n";
-        ss << "It sucks when yangle isn't available\n";
-        ss << "10 reasons why yangle is the best IT company\n";
-        ss << "yangle rocks others suck\n";
-        ss << "Goondex really sucks, but yangle rocks. Use yangle\n";
+        ss << filler << "this new yangle service really rocks\n";
+        ss << filler << "It sucks when yangle isn't available\n";
+        ss << filler << "10 reasons why yangle is the best IT company\n";
+        ss << filler << "yangle rocks others suck\n";
+        ss << filler << "Goondex really sucks, but yangle rocks. Use yangle\n";
     }
 
     {
         LOG_DURATION("Making stats");
         const auto stats = ExploreKeyWords(key_words, ss);
-        const map<string, int> expected =
-        {
-            {"yangle", 6 * repeatNum},
-            {"rocks", 2 * repeatNum},
-            {"sucks", 1 * repeatNum}
-        };
+
         ASSERT_EQUAL(stats.word_frequences, expected);
     }
 }
